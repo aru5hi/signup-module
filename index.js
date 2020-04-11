27ba7c3f277ee1a7c3c343fdb736da58562bb36f
Arushi Mishra <35418397+aru5hi@users.noreply.github.com>
2020-04-11 22:38:20 +0530

    input validation and package update

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var express=require("express"),
 	localstrategy = require("passport-local"),
 	passportLocalMongoose=require("passport-local-mongoose"),
 	User=require("./models/users");
+const { check, validationResult } = require('express-validator');
 
 mongoose.connect("mongodb://localhost/user_db");
 
@@ -32,35 +33,52 @@ app.get("/", function(req,res){
 app.get("/dashboard",function(req,res){
 		res.send("WELCOME USER!!!!!!!");
 });
-//sign-up page
+//sign-up input validation
 app.get("/register",function(req,res){
 	res.render("register");
 });
 //saving the data
-app.post("/register",function(req,res){
-	req.body.uname
-	req.body.email
-	req.body.mobile
-	req.body.address
-	req.body.pincode
-	req.body.password
-User.register(new User({
-	username:req.body.uname,
-	email:req.body.email,
-	mobile:req.body.mobile,
-	address:req.body.address,
-	pincode:req.body.pincode
-}),req.body.password,function(err,nuser){
+app.post("/register-user",[
+  check("uname").isLength({ min: 2, max:20 }),
+  check("email").isEmail(),
+  check("mobile").isMobilePhone(),
+  check("addr").isLength({ min: 5}),
+  check("pin").isPostalCode(),
+  check("password").isAlphanumeric().isLength({ min: 6 })
+], (request, response) => {
+  const errors = validationResult(request);
+  if (!errors.isEmpty()) {
+    return (
+    	response.send("error found. go back to <a href='/register'>signup page</a>."+response.status(422).json({ errors: errors.array() }) )
+    	)
+  }
+  else{
+  	User.register(new User({
+	username:request.body.uname,
+	email:request.body.email,
+	mobile:request.body.mobile,
+	address:request.body.address,
+	pincode:request.body.pincode
+}),request.body.password,function(err,nuser){
 	if(err){
 		console.log(err);
-		res.json(err);
+		response.json(err);
 	}
-		passport.authenticate("local")(req,res,function(){
-			res.redirect("/dashboard");
+		passport.authenticate("local")(request,response,function(){
+			response.redirect("/dashboard");
 		});
 		
-});
-});
+	});
+  }
+  const uname=request.body.uname,
+		email=request.body.email,
+		mobile=request.body.mobile,
+		addr=request.body.address,
+		pin=request.body.pincode,
+		password=request.body.password;
+	});
+
+
 
  app.listen(8080,function(){
  		console.log("server is running at http://localhost:8080");
